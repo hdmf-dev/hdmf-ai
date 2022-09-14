9f2846f2c4db033aff7e9eb360d5e7e3766dddb7
Andrew Tritt <ajtritt@lbl.gov>
2022-09-14 16:56:36 -0700

    Clean up methods and add tests for ResultsTable

--- hdmf_ml/results_table.py
+++ hdmf_ml/results_table.py
@@ -19,34 +19,34 @@ class ResultsTable(get_class('ResultsTable', 'hdmf-ml')):
         ret = self.__add_col('TrainValidationTestSplit', data, name, description,
                              enum=['train', 'validate', 'test'])
 
-    def add_cv_split(self, data, name="cv_split", description=None):
-        self.__add_col(data, name)
+    def add_cv_split(self, data, name="cv_split", description="cross-validation split labels"):
+        self.__add_col("CrossValidationSplit", data, name, description)
 
     def add_true_label(self, data, name="true_label", description='ground truth labels'):
         enum=False
-        if isinstance(data[0], str):
+        if isinstance(data[0], (bytes, str)):
             enc = LabelEncoder()
             data = enc.fit_transform(data)
             enum = enc.classes_
         self.__add_col('VectorData', data, name, description, enum=enum)
 
-    def add_predicted_probability(self, data, name="predicted_probability", description=None):
-        self.__add_col(data, name)
+    def add_predicted_probability(self, data, name="predicted_probability", description="the probability of the predicted class"):
+        self.__add_col('ClassProbability', data, name, description)
 
-    def add_predicted_class(self, data, name="predicted_class", description=None):
-        self.__add_col(data, name)
+    def add_predicted_class(self, data, name="predicted_class", description="the predicted class"):
+        self.__add_col('ClassLabel', data, name, description)
 
-    def add_predicted_value(self, data, name="predicted_value", description=None):
-        self.__add_col(data, name)
+    def add_predicted_value(self, data, name="predicted_value", description="the predicted regression output"):
+        self.__add_col('RegressionOutput', data, name, description)
 
-    def add_cluster_label(self, data, name="cluster_label", description=None):
-        self.__add_col(data, name)
+    def add_cluster_label(self, data, name="cluster_label", description="labels after clustering"):
+        self.__add_col('ClusterLabel', data, name, description)
 
-    def add_embedding(self, data, name="embedding", description=None):
-        self.__add_col(data, name)
+    def add_embedding(self, data, name="embedding", description="dimensionality reduction outputs"):
+        self.__add_col('EmbeddedValues', data, name, description)
 
-    def add_topk_classes(self, data, name="topk_classes", description=None):
-        self.__add_col(data, name)
+    def add_topk_classes(self, data, name="topk_classes", description="the top k predicted classes"):
+        self.__add_col('TopKClasses', data, name, description)
 
-    def add_topk_probabilities(self, data, name="topk_probabilities", description=None):
-        self.__add_col(data, name)
+    def add_topk_probabilities(self, data, name="topk_probabilities", description="the probabilityes of the top k predicted classes"):
+        self.__add_col('TopKProbabilities', data, name, description)
--- tests/test_results_table.py
+++ tests/test_results_table.py
@@ -2,8 +2,10 @@ import os
 import tempfile
 from unittest import TestCase
 
-from hdmf.common import HDF5IO, get_manager
+from hdmf.common import HDF5IO, get_manager, get_hdf5io, EnumData
 from hdmf_ml import ResultsTable
+
+import numpy as np
 
 
 class ResultsTableTest(TestCase):
@@ -16,6 +18,7 @@ class ResultsTableTest(TestCase):
             os.remove(self.path)
 
     def get_hdf5io(self):
+        #return get_hdf5io(self.path, 'w')
         return HDF5IO(self.path, 'w', manager=get_manager())
 
 
@@ -30,10 +33,87 @@ class ResultsTableTest(TestCase):
     # add_topk_classes
     # add_topk_probabilities
 
-    def test_ResultsTable(self):
+    def test_ResultsTable_tvt_split(self):
         rt = ResultsTable('foo', 'a test results table')
         rt.add_tvt_split([0, 1, 2, 0, 1])
+        with self.get_hdf5io() as io:
+            io.write(rt)
+
+    def test_ResultsTable_cv_split(self):
+        rt = ResultsTable('foo', 'a test results table')
+        rt.add_cv_split([0, 1, 2, 3, 4])
+        with self.get_hdf5io() as io:
+            io.write(rt)
+
+    def test_ResultsTable_true_label(self):
+        rt = ResultsTable('foo', 'a test results table')
         rt.add_true_label([0, 0, 0, 1, 1])
         with self.get_hdf5io() as io:
             io.write(rt)
 
+    def test_ResultsTable_true_label_str(self):
+        rt = ResultsTable('foo', 'a test results table')
+        rt.add_true_label(['a', 'a', 'a', 'b', 'b'])
+        self.assertIsInstance(rt['true_label'], EnumData)
+        np.testing.assert_array_equal(rt.true_label.elements.data, np.array(['a', 'b']))
+        with self.get_hdf5io() as io:
+            io.write(rt)
+
+
+    def test_ResultsTable_predicted_probability(self):
+        rt = ResultsTable('foo', 'a test results table')
+        rt.add_predicted_probability([[0.1, 0.9],
+                                      [0.2, 0.8],
+                                      [0.3, 0.7],
+                                      [0.4, 0.6],
+                                      [0.5, 0.5]])
+        with self.get_hdf5io() as io:
+            io.write(rt)
+
+    def test_ResultsTable_predicted_class(self):
+        rt = ResultsTable('foo', 'a test results table')
+        rt.add_predicted_class([0, 0, 1, 1, 1])
+        with self.get_hdf5io() as io:
+            io.write(rt)
+
+    def test_ResultsTable_predicted_value(self):
+        rt = ResultsTable('foo', 'a test results table')
+        rt.add_cv_split([0.0, 0.1, 0.2, 0.3, 0.4])
+        with self.get_hdf5io() as io:
+            io.write(rt)
+
+    def test_ResultsTable_cluster_label(self):
+        rt = ResultsTable('foo', 'a test results table')
+        rt.add_cv_split([0, 1, 2, 1, 0])
+        with self.get_hdf5io() as io:
+            io.write(rt)
+
+    def test_ResultsTable_embedding(self):
+        rt = ResultsTable('foo', 'a test results table')
+        rt.add_embedding([[1.1, 2.9],
+                          [1.2, 2.8],
+                          [1.3, 2.7],
+                          [1.4, 2.6],
+                          [1.5, 2.5]])
+        with self.get_hdf5io() as io:
+            io.write(rt)
+
+    def test_ResultsTable_topk_classes(self):
+        rt = ResultsTable('foo', 'a test results table')
+        rt.add_topk_classes([[1, 2],
+                             [3, 4],
+                             [5, 6],
+                             [7, 8],
+                             [9, 0]])
+        with self.get_hdf5io() as io:
+            io.write(rt)
+
+    def test_ResultsTable_topk_probabilities(self):
+        rt = ResultsTable('foo', 'a test results table')
+        rt.add_topk_probabilities([[0.9, 0.1],
+                                   [0.8, 0.2],
+                                   [0.7, 0.3],
+                                   [0.6, 0.4],
+                                   [0.5, 0.5]])
+        with self.get_hdf5io() as io:
+            io.write(rt)
