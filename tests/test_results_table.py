592544a41cf6c6c3b92153a35d36fc51a303d56b
Andrew Tritt <ajtritt@lbl.gov>
2022-09-20 16:30:42 -0700

    Use keyword args and set better test function names

--- tests/test_results_table.py
+++ tests/test_results_table.py
@@ -20,34 +20,39 @@ class ResultsTableTest(TestCase):
     def get_hdf5io(self):
         return HDF5IO(self.path, 'w', manager=get_manager())
 
-    def test_ResultsTable_diff_len(self):
-        rt = ResultsTable('foo', 'a test results table')
+    def test_add_col_diff_len(self):
+        rt = ResultsTable(name='foo', description='a test results table')
         rt.add_tvt_split([0, 1, 2, 0, 1])
         msg = ('New column true_label of length 4 is not the same length as '
                'existings columns of length 5')
         with self.assertRaisesRegex(ValueError, msg):
             rt.add_true_label([0, 0, 0, 1])
 
-    def test_ResultsTable_tvt_split(self):
+    def test_add_tvt_split(self):
         rt = ResultsTable(name='foo', description='a test results table')
         rt.add_tvt_split([0, 1, 2, 0, 1])
         with self.get_hdf5io() as io:
             io.write(rt)
 
-    def test_ResultsTable_cv_split(self):
-        rt = ResultsTable('foo', 'a test results table')
+    def test_add_cv_split(self):
+        rt = ResultsTable(name='foo', description='a test results table')
         rt.add_cv_split([0, 1, 2, 3, 4])
         with self.get_hdf5io() as io:
             io.write(rt)
 
-    def test_ResultsTable_true_label(self):
-        rt = ResultsTable('foo', 'a test results table')
+    def test_add_cv_split_bad_splits(self):
+        rt = ResultsTable(name='foo', description='a test results table')
+        with self.assertRaisesRegex(ValueError, 'Got non-integer data for cross-validation split'):
+            rt.add_cv_split([0.0, 0.1, 0.2, 0.3, 0.4])
+
+    def test_add_true_label(self):
+        rt = ResultsTable(name='foo', description='a test results table')
         rt.add_true_label([0, 0, 0, 1, 1])
         with self.get_hdf5io() as io:
             io.write(rt)
 
-    def test_ResultsTable_true_label_str(self):
-        rt = ResultsTable('foo', 'a test results table')
+    def test_add_true_label_str(self):
+        rt = ResultsTable(name='foo', description='a test results table')
         rt.add_true_label(['a', 'a', 'a', 'b', 'b'])
         self.assertIsInstance(rt['true_label'], EnumData)
         np.testing.assert_array_equal(rt.true_label.elements.data, np.array(['a', 'b']))
@@ -55,8 +60,8 @@ class ResultsTableTest(TestCase):
             io.write(rt)
 
 
-    def test_ResultsTable_predicted_probability(self):
-        rt = ResultsTable('foo', 'a test results table')
+    def test_add_predicted_probability(self):
+        rt = ResultsTable(name='foo', description='a test results table')
         rt.add_predicted_probability([[0.1, 0.9],
                                       [0.2, 0.8],
                                       [0.3, 0.7],
@@ -65,26 +70,26 @@ class ResultsTableTest(TestCase):
         with self.get_hdf5io() as io:
             io.write(rt)
 
-    def test_ResultsTable_predicted_class(self):
-        rt = ResultsTable('foo', 'a test results table')
+    def test_add_predicted_class(self):
+        rt = ResultsTable(name='foo', description='a test results table')
         rt.add_predicted_class([0, 0, 1, 1, 1])
         with self.get_hdf5io() as io:
             io.write(rt)
 
-    def test_ResultsTable_predicted_value(self):
-        rt = ResultsTable('foo', 'a test results table')
-        rt.add_cv_split([0.0, 0.1, 0.2, 0.3, 0.4])
+    def test_add_predicted_value(self):
+        rt = ResultsTable(name='foo', description='a test results table')
+        rt.add_predicted_value([0.0, 0.1, 0.2, 0.3, 0.4])
         with self.get_hdf5io() as io:
             io.write(rt)
 
-    def test_ResultsTable_cluster_label(self):
-        rt = ResultsTable('foo', 'a test results table')
-        rt.add_cv_split([0, 1, 2, 1, 0])
+    def test_add_cluster_label(self):
+        rt = ResultsTable(name='foo', description='a test results table')
+        rt.add_cluster_label([0, 1, 2, 1, 0])
         with self.get_hdf5io() as io:
             io.write(rt)
 
-    def test_ResultsTable_embedding(self):
-        rt = ResultsTable('foo', 'a test results table')
+    def test_add_embedding(self):
+        rt = ResultsTable(name='foo', description='a test results table')
         rt.add_embedding([[1.1, 2.9],
                           [1.2, 2.8],
                           [1.3, 2.7],
@@ -93,8 +98,8 @@ class ResultsTableTest(TestCase):
         with self.get_hdf5io() as io:
             io.write(rt)
 
-    def test_ResultsTable_topk_classes(self):
-        rt = ResultsTable('foo', 'a test results table')
+    def test_add_topk_classes(self):
+        rt = ResultsTable(name='foo', description='a test results table')
         rt.add_topk_classes([[1, 2],
                              [3, 4],
                              [5, 6],
@@ -103,8 +108,8 @@ class ResultsTableTest(TestCase):
         with self.get_hdf5io() as io:
             io.write(rt)
 
-    def test_ResultsTable_topk_probabilities(self):
-        rt = ResultsTable('foo', 'a test results table')
+    def test_add_topk_probabilities(self):
+        rt = ResultsTable(name='foo', description='a test results table')
         rt.add_topk_probabilities([[0.9, 0.1],
                                    [0.8, 0.2],
                                    [0.7, 0.3],
