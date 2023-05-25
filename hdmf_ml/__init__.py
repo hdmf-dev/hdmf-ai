bdb01d0c07b30bf25ab5f05b469f9cc8c63ca725
rly <rly@lbl.gov>
2023-05-25 10:04:59 -0700

    Use pyproject.toml, github actions, move things

--- hdmf_ml/__init__.py
+++ hdmf_ml/__init__.py
@@ -1,12 +1,19 @@
 from hdmf.common import load_namespaces, get_class
 
+
 def __get_resources():
-    from pkg_resources import resource_filename
-    from os.path import join
+    try:
+        from importlib.resources import files
+    except ImportError:
+        # TODO: Remove when python 3.9 becomes the new minimum
+        from importlib_resources import files
+
+    __location_of_this_file = files(__name__)
     __core_ns_file_name = 'namespace.yaml'
+    __schema_dir = 'schema'
 
     ret = dict()
-    ret['namespace_path'] = join(resource_filename(__name__, '../ml'), __core_ns_file_name)
+    ret['namespace_path'] = str(__location_of_this_file / __schema_dir / __core_ns_file_name)
     return ret
 
 
@@ -14,5 +21,4 @@ CORE_NAMESPACE = 'hdmf-ml'
 load_namespaces(__get_resources()['namespace_path'])
 
 from . import results_table
-
 from .results_table import ResultsTable
